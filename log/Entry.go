3c634ea0ad5e1dcc118e91b3249c83a0ef9483d4
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-04-26 00:27:39 +0530

    Sarthak | Adds an implementation of log Entry

---
+++ log/Entry.go
@@ -0,0 +1,60 @@
+package log
+
+import (
+	"encoding/binary"
+	"unsafe"
+)
+
+var reservedKeySize, reservedValueSize = uint32(unsafe.Sizeof(uint32(0))), uint32(unsafe.Sizeof(uint32(0)))
+var littleEndian = binary.LittleEndian
+
+type Serializable interface {
+	serialize() []byte
+}
+
+type Entry[Key Serializable] struct {
+	key   Key
+	value []byte
+}
+
+func NewEntry[Key Serializable](key Key, value []byte) *Entry[Key] {
+	return &Entry[Key]{
+		key:   key,
+		value: value,
+	}
+}
+
+func (entry *Entry[Key]) encode() []byte {
+	key := entry.key.serialize()
+	keySize, valueSize := uint32(len(key)), uint32(len(entry.value))
+
+	encoded := make([]byte, reservedKeySize+reservedValueSize+keySize+valueSize)
+	var offset uint32 = 0
+
+	littleEndian.PutUint32(encoded, keySize)
+	offset = offset + reservedKeySize
+
+	littleEndian.PutUint32(encoded[offset:], valueSize)
+	offset = offset + reservedValueSize
+
+	copy(encoded[offset:], key)
+	offset = offset + keySize
+
+	copy(encoded[offset:], entry.value)
+	return encoded
+}
+
+func (entry *Entry[Key]) decode(content []byte) ([]byte, []byte) {
+	var offset uint32 = 0
+	keySize := littleEndian.Uint32(content[offset:reservedKeySize])
+	offset = offset + reservedKeySize
+
+	valueSize := littleEndian.Uint32(content[offset:])
+	offset = offset + reservedValueSize
+
+	key := content[offset : offset+keySize]
+	offset = offset + keySize
+
+	value := content[offset : offset+valueSize]
+	return key, value
+}
---
+++ log/Entry_test.go
@@ -0,0 +1,22 @@
+package log
+
+import "testing"
+
+type serializableKey string
+
+func (key serializableKey) serialize() []byte {
+	return []byte(key)
+}
+
+func TestEncodesAKeyValuePair(t *testing.T) {
+	entry := NewEntry[serializableKey]("topic", []byte("microservices"))
+	encoded := entry.encode()
+
+	key, value := entry.decode(encoded)
+	if string(key) != "topic" {
+		t.Fatalf("Expected decoded key to be %v, received %v", "topic", string(key))
+	}
+	if string(value) != "microservices" {
+		t.Fatalf("Expected decoded value to be %v, received %v", "microservices", string(value))
+	}
+}

adbdea8b6d30c8b4a2a2a57f17f5bd6c9a960bf3
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-04-26 23:28:17 +0530

    Sarthak | Renames deletedFlag to tombstone

--- log/Entry.go
+++ log/Entry.go
@@ -8,11 +8,11 @@ import (
 
 var reservedKeySize, reservedValueSize = uint32(unsafe.Sizeof(uint32(0))), uint32(unsafe.Sizeof(uint32(0)))
 var littleEndian = binary.LittleEndian
-var deletedFlagSize = uint32(unsafe.Sizeof(byte(0)))
+var tombstoneMarkerSize = uint32(unsafe.Sizeof(byte(0)))
 
 type valueReference struct {
-	value   []byte
-	deleted byte
+	value     []byte
+	tombstone byte
 }
 
 type Entry[Key key.Serializable] struct {
@@ -23,13 +23,13 @@ type Entry[Key key.Serializable] struct {
 func NewEntry[Key key.Serializable](key Key, value []byte) *Entry[Key] {
 	return &Entry[Key]{
 		key:   key,
-		value: valueReference{value: value, deleted: 0},
+		value: valueReference{value: value, tombstone: 0},
 	}
 }
 
 func (entry *Entry[Key]) encode() []byte {
 	serializedKey := entry.key.Serialize()
-	keySize, valueSize := uint32(len(serializedKey)), uint32(len(entry.value.value))+deletedFlagSize
+	keySize, valueSize := uint32(len(serializedKey)), uint32(len(entry.value.value))+tombstoneMarkerSize
 
 	encoded := make([]byte, reservedKeySize+reservedValueSize+keySize+valueSize)
 	var offset uint32 = 0
@@ -43,7 +43,7 @@ func (entry *Entry[Key]) encode() []byte {
 	copy(encoded[offset:], serializedKey)
 	offset = offset + keySize
 
-	copy(encoded[offset:], append(entry.value.value, entry.value.deleted))
+	copy(encoded[offset:], append(entry.value.value, entry.value.tombstone))
 	return encoded
 }
 
@@ -58,6 +58,6 @@ func decode(content []byte) ([]byte, []byte) {
 	serializedKey := content[offset : offset+keySize]
 	offset = offset + keySize
 
-	value := content[offset : offset+valueSize-deletedFlagSize]
+	value := content[offset : offset+valueSize-tombstoneMarkerSize]
 	return serializedKey, value
 }

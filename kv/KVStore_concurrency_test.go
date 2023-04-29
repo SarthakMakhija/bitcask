e1fdc618d7f2b3d4276bf659865a58e9b838024d
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-04-29 15:39:48 +0530

    Sarthak | Adds concurrency tests for KVStore

---
+++ kv/KVStore_concurrency_test.go
@@ -0,0 +1,71 @@
+package kv
+
+import (
+	bitCaskConfig "bitcask/config"
+	"reflect"
+	"strconv"
+	"sync"
+	"testing"
+)
+
+func TestPutConcurrently(t *testing.T) {
+	config := bitCaskConfig.NewConfig(".", 32, 16)
+	kv, _ := NewKVStore[serializableKey](config)
+	defer kv.ClearLog()
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		_ = kv.Put("topic", []byte("microservices"))
+	}()
+	go func() {
+		defer wg.Done()
+		_ = kv.Put("disk", []byte("ssd"))
+	}()
+	go func() {
+		defer wg.Done()
+		_ = kv.Put("storage", []byte("bitcask"))
+	}()
+
+	wg.Wait()
+
+	value, _ := kv.SilentGet("topic")
+	if !reflect.DeepEqual([]byte("microservices"), value) {
+		t.Fatalf("Expected value to be %v, received %v", "microservices", string(value))
+	}
+	value, _ = kv.SilentGet("disk")
+	if !reflect.DeepEqual([]byte("ssd"), value) {
+		t.Fatalf("Expected value to be %v, received %v", "ssd", string(value))
+	}
+	value, _ = kv.SilentGet("storage")
+	if !reflect.DeepEqual([]byte("bitcask"), value) {
+		t.Fatalf("Expected value to be %v, received %v", "bitcask", string(value))
+	}
+}
+
+func TestPutConcurrentlyAcrossManyGoroutines(t *testing.T) {
+	config := bitCaskConfig.NewConfig(".", 1024, 16)
+	kv, _ := NewKVStore[serializableKey](config)
+	defer kv.ClearLog()
+
+	var wg sync.WaitGroup
+	wg.Add(100)
+	for count := 1; count <= 100; count++ {
+		go func(count int) {
+			defer wg.Done()
+			countAsString := strconv.Itoa(count)
+			_ = kv.Put(serializableKey(countAsString), []byte(countAsString))
+		}(count)
+	}
+
+	wg.Wait()
+	for count := 1; count <= 100; count++ {
+		countAsString := strconv.Itoa(count)
+		value, _ := kv.SilentGet(serializableKey(countAsString))
+		if string(value) != countAsString {
+			t.Fatalf("Expected value to be %v for the key %v, received %v", countAsString, countAsString, string(value))
+		}
+	}
+}

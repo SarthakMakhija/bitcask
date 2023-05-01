6b77de2a8f9b9a98f8178177318c4467b1c72cab
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-05-01 22:07:30 +0530

    Sarthak | Updates the code comments in Worker

--- merge/Worker.go
+++ merge/Worker.go
@@ -44,13 +44,32 @@ func (worker *Worker[Key]) start() {
 // beginMerge performs the merge operation. It is invoked ever runMergeEvery duration defined in MergeConfig
 // As a part of merge process, either all the inactive segments files are read or any of the K inactive segment files are read in memory.
 // Once those files are loaded in memory, an instance of MergedState is created that maintains a HashMap of Key and MappedStoredEntry.
-// MergedState is responsible for performing the merge operation. Merge operation is picking the latest value of a key if it is present in 2 or more segment files.
+// MergedState is responsible for performing the merge operation. Merge operation is all about picking the latest value of a key if it is present in 2 or more segment files.
 // Once the merge operation is done, the changes are written back to new inactive files and the in-memory state is updated in KeyDirectory.
-// Why do we need to update the in-memory state? Assume a Key K1 with Value V1 and Timestamp T1 is present in the segment file F1. This key was updated
-// with value V2 at a later timestamp T2 and these changes were written to a new active segment file F2. At some point in time, F2 becomes inactive.
+// Why do we need to update the in-memory state?
+// Assume a Key K1 with Value V1 and Timestamp T1 is present in the segment file F1. This key gets updated with value V2 at a later timestamp T2
+// and these changes were written to a new active segment file F2. At some point in time, F2 becomes inactive.
 // At this stage the KeyDirectory will contain the following mapping for the key K1, <K1 => {F2, Offset, EntryLength}>.
-// With this background, let's consider that the merge process starts, and it reads the contents of F1 and F2 and performs a merge. The merge writes the key
-// K1 with its new value V2 and timestamp T2 in a new file F3. The moment merge process is done, the state of Key K1 needs to be updated in the KeyDirectory.
+
+//  Segment file F1
+//	┌───────────┬──────────┬────────────┬─────┬───────┐
+//	│ T1        │ key_size │ value_size │ K1  │ V1    │
+//	└───────────┴──────────┴────────────┴─────┴───────┘
+//  Segment file F2
+//	┌───────────┬──────────┬────────────┬─────┬───────┐
+//	│ T2        │ key_size │ value_size │ K1  │ V2    │
+//	└───────────┴──────────┴────────────┴─────┴───────┘
+
+// KeyDirectory contains K1 pointing to the offset of K1 in the segment file F2.
+// With this background, let's consider that the merge process starts, and it reads the contents of F1 and F2 and performs a merge.
+// The merge writes the key K1 with its new value V2 and timestamp T2 in a new file F3, and deleted file F1 and F2.
+
+//  Segment file F3
+//	┌───────────┬──────────┬────────────┬─────┬───────┐
+//	│ T2        │ key_size │ value_size │ K1  │ V2    │
+//	└───────────┴──────────┴────────────┴─────┴───────┘
+
+// The moment merge process is done, the state of Key K1 needs to be updated in the KeyDirectory to point to the new offset in the new file.
 func (worker *Worker[Key]) beginMerge() {
 	var fileIds []uint64
 	var segments [][]*log.MappedStoredEntry[Key]
